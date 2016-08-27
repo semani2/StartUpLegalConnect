6a56d3102f7c25dc51982e31aa0c87086cfbe440
Gaurav Saraf <grv_srf@yahoo.co.in>
2016-08-27 17:33:25 -0400

    Add colors to cards

--- StartupLegalConnect/app/src/main/java/com/thestartup/startuplegalconnect/adapters/ApplicabilityAdapter.java
+++ StartupLegalConnect/app/src/main/java/com/thestartup/startuplegalconnect/adapters/ApplicabilityAdapter.java
@@ -1,6 +1,8 @@
 package com.thestartup.startuplegalconnect.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -50,11 +52,27 @@ public class ApplicabilityAdapter extends BaseAdapter {
         View rowView;
 
         rowView = inflater.inflate(R.layout.applicability_item_layout, null);
+
+        CardView card = (CardView) rowView.findViewById(R.id.applicability_item);
+        card.setCardBackgroundColor(getViewBackgroundColor(position));
+
         TextView tv = (TextView) rowView.findViewById(R.id.item_text);
         tv.setText(result[position]);
         ImageView img = (ImageView) rowView.findViewById(R.id.item_image);
         img.setImageResource(imageId[position]);
 
         return rowView;
+    }
+
+    private int getViewBackgroundColor(final int position) {
+        int rem = position % 4;
+        switch(rem)
+        {
+            case 0: return context.getResources().getColor(R.color.blue_50);
+            case 1: return context.getResources().getColor(R.color.amber_50);
+            case 2: return context.getResources().getColor(R.color.red_100);
+            case 3: return context.getResources().getColor(R.color.green_100);
+            default: return context.getResources().getColor(R.color.white);
+        }
     }
 }
