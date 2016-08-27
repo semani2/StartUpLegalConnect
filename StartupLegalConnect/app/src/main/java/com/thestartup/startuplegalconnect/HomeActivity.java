9e693d4d1a7510da60762f8385cd4a50a1450514
Sai Prajnan Emani <sai.emani@citrix.com>
2016-08-27 13:25:13 -0400

    Improvements made.

--- StartupLegalConnect/app/src/main/java/com/thestartup/startuplegalconnect/HomeActivity.java
+++ StartupLegalConnect/app/src/main/java/com/thestartup/startuplegalconnect/HomeActivity.java
@@ -1,12 +1,16 @@
 package com.thestartup.startuplegalconnect;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -16,134 +20,82 @@ import com.thestartup.startuplegalconnect.fragments.DemoFragment;
 import com.thestartup.startuplegalconnect.models.NavigationDrawerDataModel;
 
 public class HomeActivity extends AppCompatActivity {
-
-    public static final String HOME_ACTVITY = "HomeActvity";
-    private String[] mNavigationDrawerItemTitles;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
     Toolbar toolbar;
-    private CharSequence mDrawerTitle;
-    private CharSequence mTitle;
-    android.support.v7.app.ActionBarDrawerToggle mDrawerToggle;
-    private static int NUM_DRAWER_ITEMS = 6;
+    NavigationView navigationView;
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mTitle = mDrawerTitle = getTitle();
-        mNavigationDrawerItemTitles= getResources().getStringArray(R.array.navigation_drawer_items_array);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
         setupToolbar();
-
-        NavigationDrawerDataModel[] drawerItems = getNavDrawerItems();
-
-        if(getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-            getSupportActionBar().setHomeButtonEnabled(true);
-        }
-
-        DrawerItemCustomAdapter adapter = new DrawerItemCustomAdapter(this, R.layout.list_view_item_row, drawerItems);
-        mDrawerList.setAdapter(adapter);
-        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-        setupDrawerToggle();
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //Replacing the main content with ContentFragment Which is our Inbox View;
+                switch (item.getItemId()) {
+
+                    case R.id.whoWeAre:
+                        loadFragment(new AboutUs());
+                        return true;
+
+                    case R.id.whereWeFit:
+                        //loadFragment();
+                        return true;
+
+                    case R.id.commonLegalMistakes:
+                        return true;
+
+                    case R.id.stepsForStartups:
+                        return true;
+
+                    case R.id.askUs:
+                        return true;
+
+                    default:
+                        return true;
+
+                }
+            }
+        });
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,toolbar,R.string.open_drawer, R.string.close_drawer){
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        actionBarDrawerToggle.syncState();
 
         loadHomeFragment();
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
     }
 
     private void loadHomeFragment() {
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.content_frame, new AboutUs()).commit();
-
-        mDrawerList.setItemChecked(0, true);
-        mDrawerList.setSelection(0);
-        setTitle(mNavigationDrawerItemTitles[0]);
-    }
-
-    private void setupDrawerToggle() {
-        mDrawerToggle = new android.support.v7.app.ActionBarDrawerToggle(this,mDrawerLayout,toolbar,R.string.app_name, R.string.app_name);
-        mDrawerToggle.syncState();
-    }
-
-    private NavigationDrawerDataModel[] getNavDrawerItems() {
-        NavigationDrawerDataModel[] dataModels = new NavigationDrawerDataModel[NUM_DRAWER_ITEMS];
-        String[] itemNames = getResources().getStringArray(R.array.navigation_drawer_items_array);
-        // Who we are
-        dataModels[0] = new NavigationDrawerDataModel(R.mipmap.ic_people_black_24dp, itemNames[0]);
-        //Where we fit in
-        dataModels[1] = new NavigationDrawerDataModel(R.mipmap.ic_extension_black_24dp, itemNames[1]);
-        //Common legal mistakes
-        dataModels[2] = new NavigationDrawerDataModel(R.mipmap.ic_report_problem_black_24dp, itemNames[2]);
-        //Steps for start ups
-        dataModels[3] = new NavigationDrawerDataModel(R.mipmap.ic_list_black_24dp, itemNames[3]);
-        //Cyber law essentials
-        dataModels[4] = new NavigationDrawerDataModel(R.mipmap.ic_gavel_black_24dp, itemNames[4]);
-        //Ask us
-        dataModels[5] = new NavigationDrawerDataModel(R.mipmap.ic_feedback_black_24dp, itemNames[5]);
-
-        return dataModels;
     }
 
     private void setupToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        if(getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
-        }
-    }
-
-    @Override
-    public void setTitle(CharSequence title) {
-        mTitle = title;
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(mTitle);
-        }
-    }
-
-    private class DrawerItemClickListener implements ListView.OnItemClickListener {
-
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-
-    }
-
-    private void selectItem(int position) {
-
-        Fragment fragment = null;
-
-        switch (position) {
-            case 0:
-                fragment = new AboutUs();
-                break;
-            case 1:
-                //fragment = new FixturesFragment();
-                break;
-            case 2:
-                //fragment = new TableFragment();
-                break;
-
-            default:
-                break;
-        }
-
-        if (fragment != null) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
-
-            mDrawerList.setItemChecked(position, true);
-            mDrawerList.setSelection(position);
-            setTitle(mNavigationDrawerItemTitles[position]);
-            mDrawerLayout.closeDrawer(mDrawerList);
-
-        } else {
-            Log.e(HOME_ACTVITY, "Error in creating fragment");
-        }
     }
 }
